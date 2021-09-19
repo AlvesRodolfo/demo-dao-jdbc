28ba3ba4eb847408f6a0991adebb562afbdcc8e0
Rodolfo Alves <rodolfoalveseng@gmail.com>
2021-09-19 20:53:50 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,11 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory { //responsavel por instanciar os DAOs
 
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
 }
